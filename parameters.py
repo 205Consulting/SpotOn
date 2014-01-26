28612473ce98620681c8f3d9edc51523d3ed3c81
Jay Hack <jhack@stanford.edu>
2014-01-26 15:54:36 -0800

    changed a couple of things around in parameters.py to make filepaths work out

---
+++ parameters.py
@@ -0,0 +1,9 @@
+import os
+
+
+
+filenames = {
+				'data_dir':os.path.join(os.getcwd(), '../data'),
+				'calendar_df':os.path.join (os.getcwd (), '../data/calendar.df'),
+				'activity_df':os.path.join (os.getcwd(), '../data/activity_df')
+			}
